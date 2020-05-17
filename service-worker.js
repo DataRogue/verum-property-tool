2f62b997d7a5448ffd88af3b03e262c5bce3e51b
SweetBro <dmitri@bandannabandit.games>
2020-05-17 11:17:54 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/verum-property-tool/precache-manifest.80b6af176df2d6b82d93183ba673176e.js"
+  "/verum-property-tool/precache-manifest.9d1126efbd562756f1a33d46556a2af9.js"
 );
 
 self.addEventListener('message', (event) => {
